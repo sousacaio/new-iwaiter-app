7f07f215a9b5936da28af4c86c87af9388eead34
Shouganaii <frias.caio@yahoo.com.br>
2020-09-03 11:04:17 -0300

    Aplicação do renderItem do barrasmodal no checkoutmodal

--- src/components/Cart/CheckoutModal.js
+++ src/components/Cart/CheckoutModal.js
@@ -19,6 +19,8 @@ import api from '../../services/axios';
 import AsyncStorage from '@react-native-community/async-storage';
 import { connect, useDispatch, useSelector } from 'react-redux';
 import { storeOrderId } from '../../actions/cartActions';
+import AntiIcon from 'react-native-vector-icons/AntDesign';
+import MaterialComunity from 'react-native-vector-icons/MaterialCommunityIcons';
 const CheckoutModal = ({
     modalVisible,
     total,
@@ -132,6 +134,61 @@ const CheckoutModal = ({
             return 0;
         }
     });
+    const renderOrderedItems = (item, index) => {
+
+        return (
+            <View style={styles.cardItemContainer}>
+                <View style={styles.cardItemHeader}>
+                    <Text style={{ fontSize: 20, color: 'black' }}>{item.name}</Text>
+                </View>
+
+                <View style={styles.cardItemSection}>
+                    <View style={styles.cardItemIcon}>
+                        <View>
+                            <AntiIcon name="shoppingcart" size={30} />
+                        </View>
+                        <View>
+                            <Text style={styles.fontStyle}>Quantidade:</Text>
+                        </View>
+                    </View>
+
+                    <View>
+                        <Text style={styles.fontStyle}>{item.quantity}</Text>
+                    </View>
+                </View>
+                <View style={styles.cardItemSection}>
+                    <View style={styles.cardItemIcon}>
+                        <View>
+                            <MaterialComunity name="dice-1" size={30} />
+                        </View>
+                        <View>
+                            <Text style={styles.fontStyle}>Unidade:</Text>
+                        </View>
+                    </View>
+                    <View>
+                        <Text style={styles.fontStyle}>
+                            R$ {parseFloat(item.value).toFixed(2)}
+                        </Text>
+                    </View>
+                </View>
+                <View style={styles.cardItemSection}>
+                    <View style={styles.cardItemIcon}>
+                        <View>
+                            <MaterialComunity name="dice-multiple" size={30} />
+                        </View>
+                        <View>
+                            <Text style={styles.fontStyle}>Total:</Text>
+                        </View>
+                    </View>
+                    <View>
+                        <Text style={styles.fontStyle}>
+                            R$ {parseFloat(item.value * item.quantity).toFixed(2)}
+                        </Text>
+                    </View>
+                </View>
+            </View>
+        );
+    };
     return (
         <Modal animationType="slide" transparent={true} visible={modalVisible}>
             <View style={styles.modalContainer}>
@@ -143,7 +200,7 @@ const CheckoutModal = ({
                                 ellipsizeMode="tail"
                                 style={styles.nameText}>
                                 Checkout
-              </Text>
+                            </Text>
                         </View>
                     </View>
                     <View style={{ flex: 4 }}>
@@ -152,108 +209,7 @@ const CheckoutModal = ({
                                 {items.length > 0 ? (
                                     <FlatList
                                         data={items}
-                                        renderItem={({ item, index }) => (
-                                            <View
-                                                style={{
-                                                    flex: 1,
-                                                    backgroundColor: '#FAFAFA',
-                                                    margin: 10,
-                                                    borderColor: '#6200ee',
-                                                    borderBottomWidth: 6,
-                                                    borderRadius: 5,
-                                                    padding: 10,
-                                                }}>
-                                                <View
-                                                    style={{
-                                                        flex: 1,
-                                                        flexDirection: 'row',
-                                                        justifyContent: 'space-around',
-                                                        borderBottomWidth: 1,
-                                                        borderColor: '#6200ee',
-                                                    }}>
-                                                    <View
-                                                        style={{
-                                                            flex: 1,
-                                                            alignContent: 'center',
-                                                            justifyContent: 'center',
-                                                            alignItems: 'center',
-                                                        }}>
-                                                        <Text style={{ fontSize: 20 }}>Status</Text>
-                                                    </View>
-                                                    <View
-                                                        style={{
-                                                            flex: 1,
-                                                            alignContent: 'center',
-                                                            justifyContent: 'center',
-                                                            alignItems: 'center',
-                                                        }}>
-                                                        <Text style={{ fontSize: 20 }}>{item.quantity}x</Text>
-                                                    </View>
-                                                    <View
-                                                        style={{
-                                                            flex: 2,
-                                                            alignContent: 'center',
-                                                            justifyContent: 'center',
-                                                            alignItems: 'center',
-                                                        }}>
-                                                        <Text style={{ fontSize: 20 }}>{item.name}</Text>
-                                                    </View>
-                                                </View>
-                                                <View style={{ flex: 1, flexDirection: 'row' }}>
-                                                    <View
-                                                        style={{
-                                                            flex: 1,
-                                                            alignContent: 'center',
-                                                            justifyContent: 'center',
-                                                            alignItems: 'center',
-                                                        }}>
-                                                        <Tags bg="#6200ee">
-                                                            <View
-                                                                style={{
-                                                                    flex: 1,
-                                                                    alignContent: 'center',
-                                                                    justifyContent: 'center',
-                                                                    alignItems: 'center',
-                                                                }}>
-                                                                <Text style={{ fontSize: 20, color: 'white' }}>
-                                                                    Unidade
-                                </Text>
-                                                                <Text style={{ fontSize: 20, color: 'white' }}>
-                                                                    R${parseFloat(item.value).toFixed(2)}
-                                                                </Text>
-                                                            </View>
-                                                        </Tags>
-                                                    </View>
-                                                    <View
-                                                        style={{
-                                                            flex: 1,
-                                                            alignContent: 'center',
-                                                            justifyContent: 'center',
-                                                            alignItems: 'center',
-                                                        }}>
-                                                        <Tags bg="#6200ee">
-                                                            <View
-                                                                style={{
-                                                                    flex: 1,
-                                                                    alignContent: 'center',
-                                                                    justifyContent: 'center',
-                                                                    alignItems: 'center',
-                                                                }}>
-                                                                <Text style={{ fontSize: 20, color: 'white' }}>
-                                                                    Total do item:
-                                </Text>
-                                                                <Text style={{ fontSize: 20, color: 'white' }}>
-                                                                    R$
-                                  {parseFloat(
-                                                                    item.value * item.quantity,
-                                                                ).toFixed(2)}{' '}
-                                                                </Text>
-                                                            </View>
-                                                        </Tags>
-                                                    </View>
-                                                </View>
-                                            </View>
-                                        )}
+                                        renderItem={({ item, index }) => (renderOrderedItems(item, index))}
                                         keyExtractor={(item, index) => `list-item-${index}`}
                                         ListEmptyComponent={<EmptyCart />}
                                     />
@@ -277,8 +233,8 @@ const CheckoutModal = ({
                                         tsFontSizeToFit={true}
                                         style={{ color: '#A6A6A6', fontSize: 30 }}>
                                         {' '}
-                    Total
-                  </Text>
+                                        Total
+                                    </Text>
                                 </View>
                                 <View style={{ flex: 2 }}>
                                     <Text
@@ -387,6 +343,155 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         padding: 2,
+    }, cardItemContainer: {
+        flex: 1,
+        backgroundColor: '#FAFAFA',
+        margin: 10,
+        borderColor: '#7D7D7D',
+        borderRadius: 5,
+        borderWidth: 2,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+        elevation: 2,
+    },
+    cardItemHeader: {
+        flex: 1,
+        backgroundColor: '#FAFAFA',
+        borderTopColor: '#7D7D7D',
+        borderBottomColor: '#7D7D7D',
+        borderLeftColor: '#FAFAFA',
+        borderRightColor: '#FAFAFA',
+        borderWidth: 0.5,
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+    },
+    cardItemIcon: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+
+    cardItemSection: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#FAFAFA',
+        borderTopColor: '#7D7D7D',
+        borderBottomColor: '#7D7D7D',
+        borderLeftColor: '#FAFAFA',
+        borderRightColor: '#FAFAFA',
+        borderWidth: 0.5,
+        justifyContent: 'space-between',
+        padding: 10,
+    },
+    fontStyle: {
+        color: '#7D7D7D',
+        fontSize: 20,
+        fontWeight: '400',
     },
 });
 export default connect(null, null)(CheckoutModal);
+
+// <View
+// style={{
+//     flex: 1,
+//     backgroundColor: '#FAFAFA',
+//     margin: 10,
+//     borderColor: '#6200ee',
+//     borderBottomWidth: 6,
+//     borderRadius: 5,
+//     padding: 10,
+// }}>
+// <View
+//     style={{
+//         flex: 1,
+//         flexDirection: 'row',
+//         justifyContent: 'space-around',
+//         borderBottomWidth: 1,
+//         borderColor: '#6200ee',
+//     }}>
+//     <View
+//         style={{
+//             flex: 1,
+//             alignContent: 'center',
+//             justifyContent: 'center',
+//             alignItems: 'center',
+//         }}>
+//         <Text style={{ fontSize: 20 }}>Status</Text>
+//     </View>
+//     <View
+//         style={{
+//             flex: 1,
+//             alignContent: 'center',
+//             justifyContent: 'center',
+//             alignItems: 'center',
+//         }}>
+//         <Text style={{ fontSize: 20 }}>{item.quantity}x</Text>
+//     </View>
+//     <View
+//         style={{
+//             flex: 2,
+//             alignContent: 'center',
+//             justifyContent: 'center',
+//             alignItems: 'center',
+//         }}>
+//         <Text style={{ fontSize: 20 }}>{item.name}</Text>
+//     </View>
+// </View>
+// <View style={{ flex: 1, flexDirection: 'row' }}>
+//     <View
+//         style={{
+//             flex: 1,
+//             alignContent: 'center',
+//             justifyContent: 'center',
+//             alignItems: 'center',
+//         }}>
+//         <Tags bg="#6200ee">
+//             <View
+//                 style={{
+//                     flex: 1,
+//                     alignContent: 'center',
+//                     justifyContent: 'center',
+//                     alignItems: 'center',
+//                 }}>
+//                 <Text style={{ fontSize: 20, color: 'white' }}>
+//                     Unidade
+// </Text>
+//                 <Text style={{ fontSize: 20, color: 'white' }}>
+//                     R${parseFloat(item.value).toFixed(2)}
+//                 </Text>
+//             </View>
+//         </Tags>
+//     </View>
+//     <View
+//         style={{
+//             flex: 1,
+//             alignContent: 'center',
+//             justifyContent: 'center',
+//             alignItems: 'center',
+//         }}>
+//         <Tags bg="#6200ee">
+//             <View
+//                 style={{
+//                     flex: 1,
+//                     alignContent: 'center',
+//                     justifyContent: 'center',
+//                     alignItems: 'center',
+//                 }}>
+//                 <Text style={{ fontSize: 20, color: 'white' }}>
+//                     Total do item:
+// </Text>
+//                 <Text style={{ fontSize: 20, color: 'white' }}>
+//                     R$
+// {parseFloat(
+//                     item.value * item.quantity,
+//                 ).toFixed(2)}{' '}
+//                 </Text>
+//             </View>
+//         </Tags>
+//     </View>
+// </View>
+// </View>
