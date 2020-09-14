addda4a10fa3a623bcbecfd1bb3a9aadd169891d
Shouganaii <frias.caio@yahoo.com.br>
2020-09-14 11:02:57 -0300

    pesquisa nas comandas e filtrar por data

--- src/screens/History/History.js
+++ src/screens/History/History.js
@@ -16,17 +16,43 @@ import moment from "moment";
 import "moment/locale/pt-br";
 import { TouchableHighlight } from 'react-native-gesture-handler';
 import DetailOrder from './DetailOrder'
-
+import Item from './Item';
+import HistoryItem from './HistoryItem';
+import SwapIcon from 'react-native-vector-icons/Ionicons'
 
 const History = (props) => {
     const [visibilityModal, setVisibility] = useState(false)
     const [data, setData] = useState([])
+    const [search, setSearch] = useState('')
+    const [copyLastOrders, setLastOrders] = useState([])
+    const [isReversed, setIsReversed] = useState(false)
+
+    const reverseLastOrders = () => {
+        setIsReversed(!isReversed)
+    }
+    //Fazer essa função filtrar por mais parametros
+    const filterList = (list) => {
+        return list.filter(
+            (listItem) =>
+                listItem.establishment_info.name
+                    .toLowerCase()
+                    .includes(search.toLowerCase())
+
+            // ||
+            // listItem.song.toLowerCase().includes(search.toLowerCase()),
+        );
+    }
 
     const lastOrders = useSelector(state => state.customer.lastOrders)
 
     useEffect(() => {
+        console.log(data)
 
     }, [visibilityModal])
+    useEffect(() => {
+        isReversed ?
+            setLastOrders(lastOrders) : setLastOrders(lastOrders.reverse())
+    }, [isReversed])
     return (
         <View style={{ flex: 1, backgroundColor: '#6200ee' }}>
             <View style={{ flex: 1 }}>
@@ -34,31 +60,42 @@ const History = (props) => {
                     <Text style={{ color: 'white', fontSize: 40 }} >Histórico</Text>
                 </View>
             </View>
-            <View style={{ flex: 1, backgroundColor: '#6200ee' }}>
-                <TextInput style={{ flex: 1, backgroundColor: 'white', margin: 20, borderRadius: 5 }} />
+            <View style={{ flex: 1, flexDirection: 'row', backgroundColor: '#6200ee', alignItems: 'center', justifyContent: 'center' }}>
+                <TextInput
+                    onChangeText={(search) => setSearch(search)}
+                    style={{
+                        fontSize: 24,
+                        margin: 10,
+                        width: '75%',
+                        height: 60,
+                        backgroundColor: 'white',
+                        borderRadius: 10
+                    }} />
+                <SwapIcon
+                    name="swap-vertical"
+                    color="white"
+                    size={40}
+                    onPress={() => reverseLastOrders()}
+                />
             </View>
-            <View style={{ flex: 5 }}>
-                <FlatList
-                    ListHeaderComponent={<View></View>}
-                    ListFooterComponent={<View></View>}
-                    data={lastOrders}
-                    keyExtractor={(item, index) => `list-item-${index}`}
-                    renderItem={(item, index) => {
-                        //console.log(JSON.stringify(item.item, null, '\t'))
-                        const soLongSince = moment(item.item.createdAt).fromNow();
-                        const { item:
-                            {
-                                isPaid,
-                                isClosed,
-                                isCanceled,
-                                orders,
-                                establishment_info: {
-                                    name: EstName,
-                                    address: {
-                                        street,
-                                        city,
-                                        number },
-                                    photo } } } = item;
+
+            <View style={{ flex: 4, backgroundColor: '#6200ee' }}>
+                <ScrollView style={{ flex: 1, margin: 10 }}>
+                    {copyLastOrders ? filterList(copyLastOrders).map((item, index) => {
+                        const soLongSince = moment(item.createdAt).fromNow();
+                        const {
+
+                            isPaid,
+                            isClosed,
+                            isCanceled,
+                            orders,
+                            establishment_info: {
+                                name: EstName,
+                                address: {
+                                    street,
+                                    city,
+                                    number },
+                                photo } } = item;
                         const somar = (acumulado, x) => acumulado + x;
                         const valores = orders.map((item) => {
                             if (item.confirmed === 1) {
@@ -67,79 +104,30 @@ const History = (props) => {
                                 return 0;
                             }
                         });
-
                         return (
                             <TouchableHighlight onPress={() => {
-                                setData(item.item),
-                                    setVisibility(!visibilityModal)
+                                setData(item);
+                                setVisibility(!visibilityModal);
                             }}>
-                                <View style={{
-                                    flex: 1,
-                                    backgroundColor: 'white',
-                                    margin: 10,
-                                    borderRadius: 10,
-                                }}>
-                                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', margin: 10 }}>
-                                        <Text>
-                                            {isClosed ? 'Fechada' : 'Não fechada'}
-                                        </Text>
-                                        {isPaid ? <Text>
-                                            Paga
-                                        </Text> : <View></View>}
-                                        {isCanceled ? <Text>
-                                            Cancelada
-                                            </Text> : <View></View>}
+                                < HistoryItem
+                                    key={index}
+                                    isClosed={isClosed}
+                                    isPaid={isPaid}
+                                    isCanceled={isCanceled}
+                                    photo={photo}
+                                    soLongSince={soLongSince}
+                                    total={valores.reduce(somar).toFixed(2)}
+                                    name={EstName}
+                                    city={city}
+                                    street={street}
+                                    number={number}
 
-                                    </View>
-                                    <View style={{
-                                        flex: 4,
-                                        flexDirection: 'row'
-                                    }}>
-                                        <View style={{ flex: 1, margin: 10, padding: 10, justifyContent: 'space-evenly' }}>
-                                            <View style={{ flex: 1 }}>
-                                                <Image
-                                                    resizeMode={'cover'}
-                                                    style={{
-                                                        flex: 1,
-                                                        width: undefined,
-                                                        height: undefined,
-                                                        borderRadius: 5,
-                                                    }}
-                                                    source={{
-                                                        uri: photo
-                                                            ? `http://192.168.15.5:3000/${photo}`
-                                                            : 'https://i.pinimg.com/originals/48/78/6d/48786db88c95237f6e0b375dc991448a.png',
-                                                    }}
-                                                />
-                                            </View>
-                                        </View>
-                                        <View style={{ flex: 1, margin: 10, padding: 10, justifyContent: 'space-evenly' }}>
-                                            <Text>
-                                                {soLongSince}
-                                            </Text>
-                                            <Text>
-                                                R$ {valores.reduce(somar).toFixed(2)}
-                                            </Text>
-                                        </View>
-                                        <View style={{ flex: 1, margin: 10, padding: 10, justifyContent: 'space-evenly' }}>
-                                            <Text>
-                                                {EstName}
-                                            </Text>
-                                            <Text>
-                                                {city}
-                                            </Text>
-                                            <Text>
-                                                {street},{number}
-                                            </Text>
-                                        </View>
-                                    </View>
-                                </View>
-                            </TouchableHighlight>)
-                    }
-                    }
-                />
+                                />
+                            </TouchableHighlight>
+                        )
+                    }) : <View></View>}
+                </ScrollView>
             </View>
-
             {data ?
                 <DetailOrder
                     setVisibility={setVisibility}
